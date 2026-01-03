5f618f4d66bb9b55cc3ba0e13ba237337c53c96d
Abhishek Raj <abhi@raj.me>
2026-01-03 16:40:17 +0700

    Add auth routes

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,7 +1,7 @@
 import { html } from '#/lib/view'
 import { shell } from '#/lib/shell'
 
-type Props = {}
+type Props = { user?: null }
 
 export function home(props: Props) {
   return shell({
@@ -10,11 +10,31 @@ export function home(props: Props) {
   })
 }
 
-function content(props: Props) {
+function content({ user }: Props) {
   return html`<div id="root">
     <div id="header">
       <h1>onely<span class="onelyid">id</span></h1>
       <p>Permissionless Identity.</p>
+    </div>
+    <div class="container">
+      <div class="card">
+        ${user 
+          ? html`<form action="/logout" method="post" class="session-form">
+              <div>
+                Hi, <strong>@${user || 'friend'}</strong>. 
+                Hope you’re having a great day!
+              </div>
+              <div>
+                <button type="submit">Log out</button>
+              </div>
+            </form>` 
+          : html`<div class="session-form">
+              <div><a href="/login">Log in</a> to continue!</div>
+              <div>
+                <a href="/login" class="button">Log in</a>
+              </div>
+            </div>`}
+      </div>
     </div>
   </div>`
 }
--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import type { AppContext } from '#/types'
 import { page } from '#/lib/view'
 import { error } from './pages/error'
 import { home } from '#/pages/home'
+import { login } from '#/pages/login'
 
 // Helper function for defining routes
 const handler =
@@ -28,11 +29,47 @@ export const createRouter = (ctx: AppContext) => {
   // Static assets
   router.use('/public', express.static(path.join(__dirname, 'pages', 'public')))
 
+  // Login page
+  router.get(
+    '/login',
+    handler(async (req, res) => {
+      const redirectUrl = (req.query['continue'] ?? '') as string
+      return res.type('html').send(page(login({ redirectUrl })))
+    })
+  )
+
+  // Login handler
+  router.post(
+    '/login',
+    handler(async (req, res) => {
+      const handle = req.body?.handle
+      const redirectUrl = req.body?.redirectUrl
+      res.send(`handle: ${handle}<br> redirectUrl: ${redirectUrl ?? ''}`)
+    })
+  )
+
+  // Logout handler
+  router.post(
+    '/logout',
+    handler(async (_req, res) => {
+      // todo: logout
+      return res.redirect('/')
+    })
+  )
+
   // Home page
   router.get(
     '/',
     handler(async (_req, res) => {
       return res.type('html').send(page(home({})))
+    })
+  )
+
+  // User info
+  router.get(
+    '/me',
+    handler(async (_req, res) => {
+      return res.json({ user: null })
     })
   )
 
