f72e039ce869a36ded2c25139f38a9ba62c229e9
Abhishek Raj <abhi@raj.me>
2026-02-03 03:10:53 +0700

    Switch to app

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 import { cleanEnv, host, port, str, testOnly } from 'envalid'
 
-dotenv.config()
+dotenv.config({ path: '.env.app' })
 
 export const env = cleanEnv(process.env, {
   NODE_ENV: str({
@@ -11,7 +11,4 @@ export const env = cleanEnv(process.env, {
   HOST: host({ devDefault: testOnly('localhost') }),
   PORT: port({ devDefault: testOnly(3000) }),
   PUBLIC_URL: str({}),
-  DB_PATH: str({ devDefault: ':memory:' }),
-  COOKIE_SECRET: str({ devDefault: '00000000000000000000000000000000' }),
-  STATE_SECRET: str({ devDefault: '00000000000000000000000000000000' }),
 })
