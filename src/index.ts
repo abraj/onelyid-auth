abraj/onelyid-app

Use oauth client middleware

src/index.ts
@@ -2,6 +2,7 @@ import events from 'node:events'
 import type http from 'node:http'
 import express, { type Express } from 'express'
 import { pino } from 'pino'
+import { authMiddleware, type MiddlewareConfig } from '@onelyid/client'
 
 import { env } from '#/lib/env'
 import { createRouter } from '#/routes'
@@ -29,6 +30,16 @@ export class Server {
     const router = createRouter(ctx)
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
+
+    const config: MiddlewareConfig = {
+      // dbPath: env.DB_PATH,
+      // cookieSecret: env.COOKIE_SECRET,
+      // publicUrl: env.PUBLIC_URL,
+      // logger,
+      // loginRedirect: '/',
+    };
+    app.use(authMiddleware(config))
+
     app.use(router)
     app.use((_req, res) => res.sendStatus(404))
 

abraj/onelyid-auth

src/index.ts
@@ -2,6 +2,7 @@ import events from 'node:events'
 import type http from 'node:http'
 import express, { type Express } from 'express'
 import { pino } from 'pino'
+import { authMiddleware, type AuthMiddlewareConfig } from '@onelyid/client'
 
 import { env } from '#/lib/env'
 import { createRouter } from '#/routes'
@@ -27,6 +28,16 @@ export class Server {
 
     // Routes & middlewares
     const router = createRouter(ctx)
+
+    const config: AuthMiddlewareConfig = {
+      // dbPath: env.DB_PATH,
+      // cookieSecret: env.COOKIE_SECRET,
+      // publicUrl: env.PUBLIC_URL,
+      // mountPath: '/auth',
+      // loginRedirect: '/',
+      // logger,
+    };
+    app.use(authMiddleware(config))
 
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))