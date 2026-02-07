1a5bd48f31833ba53ff726903729a83071832e9f
Abhishek Raj <abhi@raj.me>
2026-02-08 03:55:47 +0700

    t1.2

--- src/routes.ts
+++ src/routes.ts
@@ -67,18 +67,6 @@ export const createRouter = (ctx: AppContext) => {
     })
   )
 
-  // // Login handler
-  // router.post(
-  //   '/login',
-  //   handler(async (req, _res) => {
-  //     const handle = req.body?.handle
-  //     const redirectUrl = req.body?.redirectUrl
-  //     if (handle) {
-  //       await req.authFlow(handle, redirectUrl)
-  //     }
-  //   })
-  // )
-
   // Logout handler
   router.post(
     '/logout',
